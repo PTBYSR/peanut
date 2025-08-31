4a73db22d0c5b2e25421d16868489a0ce3424c0b
PTBYSR <paul.emechebe@gmail.com>
2025-08-31 22:43:57 +0100

    omo

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="">
       <main className="">
-        <nav className="border flex justify-center items-center ">
+        <nav className="flex justify-center items-center ">
           <Image
             className="absolute hidden md:block  md:top-[0.2px] -z-[1]"
             src={"/nav.png"}
@@ -21,9 +21,9 @@ export default function Home() {
             width={500}
             height={200}
           />
-          <div className="w-full flex justify-center items-center">
-            <div className="flex px-[20px] w-full justify-between items-center border border-pink-500 md:w-[70%]">
-              <div id="logo" className="md:border md:w-auto w-[40px]">
+          <div className=" pt-[5px] w-full flex justify-center items-center">
+            <div className="flex px-[20px] w-full justify-between items-center  md:w-[70%]">
+              <div id="logo" className="md:w-auto w-[40px]">
                 <Image src={"/logo.png"} width={60} height={60} />
               </div>
 
@@ -39,6 +39,8 @@ export default function Home() {
                 </Link>
               </div>
 
+              <div className=" flex ">
+
               <div id="buy" className="md:w-auto w-[120px]">
                 <Image src={"/buy.png"} width={200} height={100} />
               </div>
@@ -46,20 +48,26 @@ export default function Home() {
               <div id="x" className="md:w-auto w-[60px]">
                 <Image src={"/x.png"} width={100} height={100} />
               </div>
+              <div id="x" className="md:w-auto w-[60px]">
+                <Image src={"/hamburger-menu-mb.svg"} width={100} height={100} />
+              </div>
+              </div>
+
+              
             </div>
           </div>
         </nav>
 
-        <div className="h-[600px] border-2  mt-[50px] justify-center items-center flex relative">
+        <div className="h-[600px] mt-[50px] justify-center items-center flex relative">
           <Image
-            className="-z-[1] absolute md:hidden hidden  top-[100px] border"
+            className="-z-[1] absolute md:hidden hidden  top-[100px] b"
             src={"/hero.svg"}
             alt="hero"
             width={1000}
             height={1000}
           />
 
-          <div className=" mt-[26%] ml-[40px] hidden flex-row justify-between border w-[40%]">
+          <div className=" mt-[26%] ml-[40px] hidden flex-row justify-between  w-[40%]">
             <div className="animate-tilt">
               <Image src={"/button2.png"} width={130} height={130} />
             </div>
